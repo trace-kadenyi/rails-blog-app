e9b6d230915fecc007be13f334eb6c4079039078
kadenyi <treykadenyi@gmail.com>
2022-10-20 14:16:12 +0300

    Add more unit tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :users, foreign_key: 'user_id', class_name: 'User'
   belongs_to :posts, foreign_key: 'post_id', class_name: 'Post'
-  after_create :update_comments_counter
+  after_save :update_comments_counter
 
   def update_comments_counter
-    posts.increment!(:comments_counter)
+    posts.update(comments_counter: posts.comments_counter + 1)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :users, foreign_key: 'user_id', class_name: 'User'
   belongs_to :posts, foreign_key: 'post_id', class_name: 'Post'
-  after_create :update_likes_counter
+  after_save :update_likes_counter
+
+  private
 
   def update_likes_counter
     posts.increment!(:likes_counter)
