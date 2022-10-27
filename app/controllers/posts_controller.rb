33edca1e29745bcbc4e40162bf2952e3d4ee3635
kadenyi <treykadenyi@gmail.com>
2022-10-27 16:32:24 +0300

    Add the new and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @user = current_user
+    @post.users = @user
+
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
