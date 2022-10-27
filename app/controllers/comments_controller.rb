0e5590b4ecd6de51b13ad88b6e36417162061ca0
kadenyi <treykadenyi@gmail.com>
2022-10-27 16:25:01 +0300

    Add comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @user = current_user
+    @comment.users = @user
+
+    @comment.save unless @comment.invalid?
+    redirect_to user_posts_path(@user)
+  end
+
+    def comment_params
+      params.require(:comment).permit(:text, :post_id)
+    end
+end
