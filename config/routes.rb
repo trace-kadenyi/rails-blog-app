105700fb27d2a7c0973318501d71fcd7791eea6e
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-01 14:26:15 +0100

    Configure user registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show] do
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+  root to: "users#index"
+
+  resources :users, only: [:index, :show, :new, :create] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:index, :show, :new, :create]
       resources :likes, only: [:new, :create]
     end
   end
- 
-  root to: "users#index"
 end
