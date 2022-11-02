7a102b2d656633be5dc1c9f1f27d1d095abba41e
kadenyi <treykadenyi@gmail.com>
2022-11-02 23:02:21 +0300

    Add the dependent: destroy property

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
   after_create :update_posts_counter
 
