4720f13938746e16b23a362229a31f344fadabaf
kadenyi <treykadenyi@gmail.com>
2022-11-03 01:41:09 +0300

    Add permissions to User in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,16 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    can :read, Post
+
+    return unless user.present?
+
+    can :manage, Post, user_id: user.id
+    can %i[create destroy], Comment
+
+    return unless user.role == 'admin'
+
+    can :manage, :all
+  end
 end
