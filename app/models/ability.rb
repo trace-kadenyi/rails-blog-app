351323d1ed6b30264f3b0b114c3c4005aa15194d
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-03 11:23:29 +0100

    Resolve authorization errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Post
+    can %i[create read], Post
+    can %i[create read], Comment
+    can %i[update destroy], Post, user_id: user.id
+    can %i[update destroy], Comment, user_id: user.id
 
-    return unless user.present?
-
-    can :manage, Post, user_id: user.id
-    can %i[create destroy], Comment
-
-    return unless user.role == 'admin'
+    return unless user.admin?
 
     can :manage, :all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :bio, presence: true, length: { minimum: 5, maximum: 180 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
